e6ad752e9a85b49a839bb91e0943cd2fdbfa68b3
SPAWNS <1835939062@qq.com>
2025-09-02 20:25:42 +0800

    Update index.ts

--- metadata/index.ts
+++ metadata/index.ts
@@ -1,7 +1,7 @@
 /** 文本 */
-export const siteName = 'Nólëbase'
-export const siteShortName = 'Nólëbase'
-export const siteDescription = '记录回忆，知识和畅想的地方'
+export const siteName = 'SPAWNS'
+export const siteShortName = '知识库'
+export const siteDescription = '宝山居士的知识库'
 
 /** 文档所在目录 */
 export const include = ['笔记', '生活']
