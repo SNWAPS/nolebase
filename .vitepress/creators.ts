3d4b8086629883410a3234fb0752ed96851e4e75
SPAWNS <1835939062@qq.com>
2025-09-03 13:00:58 +0800

    Update creators.ts

--- .vitepress/creators.ts
+++ .vitepress/creators.ts
@@ -20,17 +20,18 @@ const getAvatarUrl = (name: string) => `https://github.com/${name}.png`
 
 export const creators: Creator[] = [
   {
-    name: '絢香猫',
-    avatar: '',
-    username: 'nekomeowww',
-    title: 'Nólëbase 原始创作者',
+    name: '宝山居士',
+    avatar: '123',
+    username: 'SPAWNS',
+    title: '大傻逼',
     desc: '开发者，专注于基础设施维护，数据分析，后端、DevOps 开发',
     links: [
-      { type: 'github', icon: 'github', link: 'https://github.com/nekomeowww' },
-      { type: 'twitter', icon: 'twitter', link: 'https://twitter.com/ayakaneko' },
+      { type: 'github', icon: 'github', link: 'https://github.com/zgh2825902029' },
+      { type: 'twitter', icon: 'twitter', link: '' },
+      { type: 'email', icon: 'email', link: '1835939062@qq.com' },
     ],
-    nameAliases: ['nekomeowww', '绚香猫', '絢香猫', 'Neko Ayaka', 'Ayaka Neko'],
-    emailAliases: ['neko@ayaka.moe'],
+    nameAliases: ['0', '1', '2', '3', '4'],
+    emailAliases: ['1835939062@qq.com'],
   },
 ].map<Creator>((c) => {
   c.avatar = c.avatar || getAvatarUrl(c.username)
