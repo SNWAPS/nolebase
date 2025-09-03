2d64bc8d60b7f18e5f92812ab300c3ca9393fcf8
SPAWNS <1835939062@qq.com>
2025-09-03 13:52:19 +0800

    Update index.ts

--- metadata/index.ts
+++ metadata/index.ts
@@ -1,7 +1,7 @@
 /** 文本 */
-export const siteName = '焚决'
-export const siteShortName = '焚决'
-export const siteDescription = '焚决'
+export const siteName = 'SPAWNS知识库'
+export const siteShortName = 'SPAWNS知识库'
+export const siteDescription = 'SPAWNS知识库'
 
 /** 文档所在目录 */
 export const include = ['笔记', '生活']
