1fa143b1e0c1dcb3dbf6c61e144dda0de2b5fcb0
SPAWNS <1835939062@qq.com>
2025-09-03 12:33:10 +0800

    Update index.ts

--- metadata/index.ts
+++ metadata/index.ts
@@ -1,7 +1,7 @@
 /** 文本 */
-export const siteName = '宝山居士的知识库'
-export const siteShortName = '宝山居士的知识库'
-export const siteDescription = '宝山居士的知识库'
+export const siteName = '焚决'
+export const siteShortName = '焚决'
+export const siteDescription = '焚决'
 
 /** 文档所在目录 */
 export const include = ['笔记', '生活']
@@ -9,7 +9,7 @@ export const include = ['笔记', '生活']
 /** Repo */
 export const githubRepoLink = 'https://github.com/zgh2825902029/SPAWNS'
 /** Discord */
-export const discordLink = 'https://discord.gg/XuNFDcDZGj'
+export const discordLink = ''
 
 /** 无协议前缀域名 */
 export const plainTargetDomain = 'spawns.cn'
