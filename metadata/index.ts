7704ec2652950be796e8b06ee799ff256580672d
SPAWNS <1835939062@qq.com>
2025-09-16 18:47:46 +0800

    1

--- metadata/index.ts
+++ metadata/index.ts
@@ -4,7 +4,7 @@ export const siteShortName = 'SPAWNS的数据库'
 export const siteDescription = 'SPAWNS的数据库'
 
 /** 文档所在目录 */
-export const include = ['数据库','数据库1']
+export const include = ['数据库','阿萨德']
 /** Repo */
 export const githubRepoLink = 'https://github.com/SNWAPS'
 /** Discord */
